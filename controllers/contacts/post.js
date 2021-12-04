fefac6a6082c4b2d70acf57da38b5af225b1162b
AleksandrTsyhankov <74210910+AleksandrTsyhankov@users.noreply.github.com>
2021-12-04 23:09:22 +0200

    Squashed commit of the following:
    commit b32fd6ae78987f66d44952d170cff500ccf3bc3f
    Author: AleksandrTsyhankov <74210910+AleksandrTsyhankov@users.noreply.github.com>
    Date:   Sat Dec 4 23:05:21 2021 +0200
    
        Update post.js
    
    commit 1cd9c1665bcd2ef6fc7dcb2eca950ef296fb350d
    Author: AleksandrTsyhankov <74210910+AleksandrTsyhankov@users.noreply.github.com>
    Date:   Sat Dec 4 21:53:29 2021 +0200
    
        Update post.js
    
    commit 98aec22f79ef8ae55621f38ffe34c25e548fc200
    Author: AleksandrTsyhankov <74210910+AleksandrTsyhankov@users.noreply.github.com>
    Date:   Sat Dec 4 21:40:34 2021 +0200
    
        hw5 3rd commit
    
    commit 8029dd80c1b482b92cfb3121aeedac02af1549b7
    Author: AleksandrTsyhankov <74210910+AleksandrTsyhankov@users.noreply.github.com>
    Date:   Sat Dec 4 21:29:59 2021 +0200
    
        hw5 2nd commit
    
    commit 8901d1656b234c5cab8e986f898bbbf87f1e46b8
    Author: AleksandrTsyhankov <74210910+AleksandrTsyhankov@users.noreply.github.com>
    Date:   Sat Dec 4 20:27:19 2021 +0200
    
        hw5 commit
    
    commit e9990114a8aadd88a4de018f26d7387ab9cc5fb6
    Author: AleksandrTsyhankov <74210910+AleksandrTsyhankov@users.noreply.github.com>
    Date:   Sat Dec 4 11:24:52 2021 +0200
    
        Squashed commit of the following:
    
        commit e8b5b816ab808125e5afc380833bb7947b8559e7
        Merge: 8f525cb 4df8b96
        Author: AleksandrTsyhankov <74210910+AleksandrTsyhankov@users.noreply.github.com>
        Date:   Sat Dec 4 11:06:49 2021 +0200
    
            Merge pull request #3 from AleksandrTsyhankov/hw04-auth
    
            hw4 commit
    
        commit 4df8b963cb5bf89953b96a1cfc5380e7a127bc79
        Author: AleksandrTsyhankov <74210910+AleksandrTsyhankov@users.noreply.github.com>
        Date:   Thu Dec 2 00:24:52 2021 +0200
    
            hw4 commit

--- controllers/contacts/post.js
+++ controllers/contacts/post.js
@@ -3,11 +3,11 @@ const { contacts } = require('../../models/index')
 const fs = require('fs/promises');
 const path = require('path');
 
-const contactsDir = path.join(__dirname, '../../public/contacts');
+const contactsDir = path.join(__dirname, '../../public/avatars');
 
 const post = async (req, res, next) => {
   const avatar = gravatar.url('aleksandr@mail.com')
-  const newContact = { ...req.body, owner: req.user._id }
+  const newContact = { ...req.body, owner: req.user._id, avatar }
   const result = await contacts.Contact.create({ ...newContact, avatar })
 
   const contactsFolder = path.join(contactsDir, String(result._id));
